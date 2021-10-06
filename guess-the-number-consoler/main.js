2f239fc13d581d42d6f0fd59b8a376c1c3f26531
sarahdridi8 <sarah.drid@gmail.com>
2021-10-06 11:33:14 +0200

    update

--- guess-the-number-consoler/main.js
+++ guess-the-number-consoler/main.js
@@ -11,34 +11,37 @@ const submit = (x) => {
     x = parseFloat (x)
 
     if (Number.isNaN(x)) {
-        console.log('TWrong input, plaese give a number')
-        document.body.innerHTML = '<h1>⚠️</h1>'
+        console.log('Wrong input, please give a number.')
+        document.body.innerHTML = '<h1 class="bounce">⚠️</h1>'
         document.body.style.backgroundColor = 'red'
-    }
 
-    else if (x < 0 || x >100) {
+    
+    } else if (x < 0 || x >100) {
         console.log('Number should be between 0 & 100')
         document.body.innerHTML = '<h1>Number should be between 0 & 100.</h1>'
         document.body.style.backgroundColor = '#red'
 
-    else if (x < mysteryNumber) {
+    } else if (x < mysteryNumber) {
         console.log('Too small.')
-        document.body.innerHTML = '<h1>Too small.</h1>'
+        document.body.innerHTML = '<h1 class="bounce">Too small.</h1>'
         document.body.style.backgroundColor = '#801854'
 
-    }
-    else if (x > mysteryNumber) {
+    } else if (x > mysteryNumber) {
         console.log('Too big.')
-        document.body.innerHTML = '<h1>Too big.</h1>'
+        document.body.innerHTML ='<h1 class="bounce">Too big.</h1>'
         document.body.style.backgroundColor = '#2b1880'
-    }
-    else if (x == mysteryNumber) {
+
+    }  else if (x == mysteryNumber) {
         console.log('Hurrah')
-        document.body.innerHTML = '<h1>Hurrah!</h1>'
+        document.body.innerHTML = '<h1 class="bounce">Hurrah!</h1>'
         document.body.style.backgroundColor = '#428018'
     }
 }
 
 const cheat = () => {
     console.log(`The mystery number is ${mysteryNumber}`)
-}
+  }
+  
+  const distance = (x, y) => {
+    return 
+  }
